009602e8b70cb9db1b22b8530a2678128da00197
Arish-Shah <arish0246@gmail.com>
2019-07-26 19:39:08 +0530

    Updates

---
+++ precache-manifest.8cc9f2570e60f5d85f87ee943aa1bfea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f2c16c45f714fe907a84cfbdf6c92c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d674c537fff5efebfc3",
+    "url": "/static/css/main.294060b8.chunk.css"
+  },
+  {
+    "revision": "03e76f3b690b75cfbb29",
+    "url": "/static/js/2.7bcd5fc5.chunk.js"
+  },
+  {
+    "revision": "2d674c537fff5efebfc3",
+    "url": "/static/js/main.79588157.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/URL.js
+++
@@ -1,2 +0,0 @@
-export const url = 'https://github.com/Arish-Shah/';
-export const twitterUrl = 'https://twitter.com/Arish_Shah';
