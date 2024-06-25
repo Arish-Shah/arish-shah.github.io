267d7a850d3b2d90924c2cf6ad0df613c60bcec1
Arish-Shah <arish.r.shah@gmail.com>
2024-06-25 20:19:01 +0200

    feat: initial commit

---
+++ .prettierrc.mjs
@@ -0,0 +1,17 @@
+/** @type {import("prettier").Config} */
+export default {
+  endOfLine: "lf",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# arish-shah.github.io
+
+personal website
